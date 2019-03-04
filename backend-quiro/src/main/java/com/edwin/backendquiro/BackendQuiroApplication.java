c9505c4d4369b09a1e4f7165c067673a975b2d30
edwin <villa-edwin@hotmail.com>
2019-03-04 11:19:48 +0100

    iniciar proyecto

---
+++ backend-quiro/src/main/java/com/edwin/backendquiro/BackendQuiroApplication.java
@@ -0,0 +1,13 @@
+package com.edwin.backendquiro;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BackendQuiroApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BackendQuiroApplication.class, args);
+	}
+
+}
